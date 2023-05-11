1364ae5ff95a224705a4d0de6f5dd9155b30c8db
Leo Pinetzki <leo.leo11@hotmail.de>
2023-05-11 10:25:47 +0200

    update download script

--- download_data.sh
+++ download_data.sh
@@ -4,18 +4,18 @@ cd data
 mkdir images
 
 cd images
-wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/3918/3/frame-irg-003918-3-0213.jpg
-wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/3918/3/frame-r-003918-3-0213.fits.bz2
-wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/2505/3/frame-g-003918-3-0213.fits.bz2
-wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/2505/3/frame-i-003918-3-0213.fits.bz2
-wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/2505/3/frame-u-003918-3-0213.fits.bz2
-wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/2505/3/frame-z-003918-3-0213.fits.bz2
+wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/3918/3/frame-irg-003918-3-0213.jpg -nc
+wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/frame-r-008162-6-0080.fits.bz2 -nc
+wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/frame-g-008162-6-0080.fits.bz2 -nc
+wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/frame-i-008162-6-0080.fits.bz2 -nc
+wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/frame-u-008162-6-0080.fits.bz2 -nc
+wget https://data.sdss.org/sas/dr17/eboss/photoObj/frames/301/8162/6/frame-z-008162-6-0080.fits.bz2 -nc
 
 cd ..
 
 mkdir labels
 cd labels
-wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-003918-3-gal.fits.gz
-wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-003918-3-star.fits.gz
-wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-003918-3-sky.fits.gz
+wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-003918-3-gal.fits.gz -nc
+wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-003918-3-star.fits.gz -nc
+wget https://data.sdss.org/sas/dr17/eboss/sweeps/dr13_final/301/calibObj-003918-3-sky.fits.gz -nc
 

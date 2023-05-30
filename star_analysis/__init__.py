1b58a65ce8859369425af9d1cf2e6470bdb7593b
Leo Pinetzki <leo.leo11@hotmail.de>
2023-05-30 15:26:31 +0200

    added logging

--- star_analysis/__init__.py
+++ star_analysis/__init__.py
@@ -1,5 +1,5 @@
 import logging
 logger = logging.getLogger(__name__)
-FORMAT = "[%(levelname)s] %(filename)s:%(funcName)s:%(lineno)s: %(message)s"
+FORMAT = "[%(levelname)s][%(asctime)s] %(filename)s:%(funcName)s:%(lineno)s: %(message)s"
 logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.DEBUG)
--- star_analysis/dataprovider/image_downloader.py
+++ star_analysis/dataprovider/image_downloader.py
@@ -6,7 +6,7 @@ from typing import Optional
 import requests
 import bs4
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger(__name__)
 
 
 class ImageDownloader:
@@ -19,15 +19,17 @@ class ImageDownloader:
 
         self.__loaded_images = 0
 
-    def download(self, to: str) -> tuple[list[str], list[str]]:
+    def download(self, to: str, only_labels: bool = False) -> tuple[list[str], list[str]]:
         image_path = os.path.join(to, 'images')
         label_path = os.path.join(to, 'labels')
 
         os.makedirs(image_path, exist_ok=True)
         os.makedirs(label_path, exist_ok=True)
 
-        loaded_images = self.__get_images(
-            ImageDownloader.URL, image_path)
+        loaded_images = []
+        if not only_labels:
+            loaded_images = self.__get_images(
+                ImageDownloader.URL, image_path)
 
         self.__loaded_images = 0
         loaded_labels = self.__get_images(
@@ -38,10 +40,14 @@ class ImageDownloader:
     def __combine_url(self, base: str, extension: str) -> str:
         return f"{base}{'/' if not base.endswith('/') and not extension.startswith('/') else ''}{extension}"
 
+    def __is_file(self, name: str) -> bool:
+        return name.endswith('.bz2') or name.endswith('.gz')
+
     def __get_images(self, url: str, folder: str) -> list[str]:
         if self.__limit is not None and self.__loaded_images >= self.__limit:
             return []
 
+        logger.info(f"Getting images from {url}")
         base_table = requests.get(url)
 
         html_data = bs4.BeautifulSoup(base_table.content)
@@ -52,10 +58,10 @@ class ImageDownloader:
                        ]
 
         loaded_images = self.__download_urls(
-            [self.__combine_url(url, i) for i in table_links if i.endswith('.bz2')], folder)
+            [self.__combine_url(url, i) for i in table_links if self.__is_file(i)], folder)
 
         now_final_images = []
-        for non_final_link in [i for i in table_links if not i.endswith('.bz2')]:
+        for non_final_link in [i for i in table_links if not self.__is_file(i)]:
             now_final_images.extend(self.__get_images(
                 self.__combine_url(url, non_final_link), folder)
             )

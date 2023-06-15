9c271d699a78739627b561dac7fe975283da8f9c
clemens <kubach@campus.tu-berlin.de>
2023-06-15 12:31:47 +0200

    add datamodule test interfaces

---
+++ tests/data/datamodule_tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class DataModuleTestCase(unittest.TestCase):
+    def test_loading(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_exclusivity(self):
+        pass
+
+    def test_deterministic_shuffling(self):
+        pass
+
+    def test_equal_label_distribution(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

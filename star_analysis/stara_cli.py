2992c20aa915fb1ff6fc25398c7ea769accf5a4f
clemens <kubach@campus.tu-berlin.de>
2023-06-16 19:44:07 +0200

    add wip cli

---
+++ star_analysis/stara_cli.py
@@ -0,0 +1,30 @@
+import fire as fire
+
+from star_analysis.runner.sdss_runner import SdssRunner
+
+
+class StaraCLI:
+    """The command line interface for the Star Analysis package."""
+
+    class ml(SdssRunner):
+        pass
+
+    class dataset:
+        def download(self):
+            """Downloads the SDSS dataset."""
+            from star_analysis.utils.download_dataset import download_sdss_dataset
+            download_sdss_dataset()
+
+        def repair(self):
+            """Repairs your local files."""
+            from star_analysis.utils.repair_dataset import repair_data
+            repair_data()
+
+        def align(self):
+            """Aligns the downloaded fits files."""
+            from star_analysis.utils.alignsave_dataset import align_data
+            align_data()
+
+
+if __name__ == '__main__':
+    fire.Fire(StaraCLI)

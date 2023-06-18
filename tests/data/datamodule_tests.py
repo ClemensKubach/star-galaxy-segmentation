30183e25043980190477c6c4c4fe5c3f2e500cb9
clemens <kubach@campus.tu-berlin.de>
2023-06-18 20:25:05 +0200

    wip datamodule tests

--- tests/data/datamodule_tests.py
+++ tests/data/datamodule_tests.py
@@ -1,6 +1,8 @@
+import os
 import unittest
 
 import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from star_analysis.data.configs import SdssDatasetConfig, SdssDataModuleConfig
@@ -11,9 +13,9 @@ from star_analysis.dataprovider.sdss_dataprovider import SDSSDataProvider
 class DataModuleTestCase(unittest.TestCase):
     def setUp(self):
         self.batch_size = 32
-        self.shuffle_train = True
+        self.shuffle_train = False
         dataset_config = SdssDatasetConfig(
-            patch_shape=(5, 5),
+            patch_shape=None,
             prepare=True,
             run=SDSSDataProvider.FIXED_VALIDATION_RUN
         )
@@ -22,7 +24,7 @@ class DataModuleTestCase(unittest.TestCase):
             batch_size=self.batch_size,
             shuffle_train=self.shuffle_train,
             train_size=0.8,
-            num_workers=1
+            num_workers=4
         )
         self.datamodule = SdssDataModule(module_config)
         self.datamodule.prepare_data()
@@ -30,37 +32,69 @@ class DataModuleTestCase(unittest.TestCase):
 
     def test_loading(self):
         train_loader = self.datamodule.train_dataloader()
-        train_loader_iter = iter(train_loader)
-        batch_x, batch_y = next(train_loader_iter)
-        assert batch_x.shape == (self.batch_size, 5, 5)
-        #assert batch_y.shape == (self.batch_size, 5, 5)
+        for e in train_loader:
+            x, y = e
+            assert x.shape == (self.batch_size, 224, 224, 5)
+            assert y.shape == (self.batch_size, 224, 224, 2)
+            break
 
-    def test_exclusivity(self):
+    def test_exclusivity_train_subsets(self):
         train_loader = self.datamodule.train_dataloader()
         val_loader = self.datamodule.val_dataloader()
-        test_loader = self.datamodule.test_dataloader()
 
         train_set = set(train_loader.dataset.indices)
         val_set = set(val_loader.dataset.indices)
-        test_set = set(test_loader.dataset.indices)
-        in_all = train_set & val_set & test_set
-        assert len(in_all) == 0
+        in_both = train_set & val_set
+        assert len(in_both) == 0
 
-    def test_exclusivity_on_sample(self):
+    def test_exclusivity_test_set(self):
+        test_loader = self.datamodule.test_dataloader()
         train_loader = self.datamodule.train_dataloader()
         val_loader = self.datamodule.val_dataloader()
+        for e in tqdm(test_loader.dataset):
+            x, _ = e
+            assert self.__value_exclusivity(test_loader, x) == 1
+            assert self.__value_exclusivity(train_loader, x) == 0
+            assert self.__value_exclusivity(val_loader, x) == 0
+
+    def test_exclusivity_train_set(self):
+        loader = DataLoader(
+            self.datamodule.train_dataset,
+            batch_size=1,
+            num_workers=1,
+            shuffle=False,
+            pin_memory=True,
+            persistent_workers=True,
+        )
+
+        o = []
+        for e in tqdm(loader):
+            x, _ = e
+            #assert self.__value_exclusivity(test_loader, x) == 0
+            #assert self.__value_exclusivity(train_loader, x) == 1
+            #assert self.__value_exclusivity(val_loader, x) == 0
+            t = self.__value_exclusivity(self.datamodule.train_dataloader(), x)
+            o.append(t)
+        print(o)
+
+    def test_exclusivity_val_set(self):
         test_loader = self.datamodule.test_dataloader()
+        train_loader = self.datamodule.train_dataloader()
+        val_loader = self.datamodule.val_dataloader()
+        for e in tqdm(val_loader.dataset):
+            x, _ = e
+            assert self.__value_exclusivity(test_loader, x) == 0
+            assert self.__value_exclusivity(train_loader, x) == 0
+            assert self.__value_exclusivity(val_loader, x) == 1
 
-        last_train_item = train_loader.dataset[-1]
-        x, _ = last_train_item
-        for i in tqdm(range(len(val_loader.dataset))):
-            x_val, _ = val_loader.dataset[i]
-            assert torch.equal(x, x_val)
-
-        for i in tqdm(range(len(test_loader.dataset))):
-            x_test, _ = test_loader.dataset[i]
-            assert torch.equal(x, x_test)
-
+    def __value_exclusivity(self, loader: DataLoader, x_compare: torch.Tensor):
+        num_equals = 0
+        for batch in loader:
+            xb, _ = batch
+            for x in xb:
+                if torch.equal(x, x_compare):
+                    num_equals += 1
+        return num_equals
 
     def test_deterministic_shuffling(self):
         pass
@@ -70,7 +104,4 @@ class DataModuleTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    #unittest.main()
-    d = DataModuleTestCase()
-    d.setUp()
-    d.test_exclusivity_on_sample()
+    unittest.main()

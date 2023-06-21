24aaeea07ebbfd43d35963b1956c274a73c26046
Leo Pinetzki <leo.leo11@hotmail.de>
2023-06-21 22:17:49 +0200

    added augmentation

--- star_analysis/data/datasets.py
+++ star_analysis/data/datasets.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import torch
 import torch.utils.data as data
 from torch import Tensor
 
@@ -21,7 +22,8 @@ class Sdss(data.Dataset):
         self.inner_padding = INNER_IMAGE_PADDING
 
         if config.data_dir and config.data_dir != SDSSDataProvider.SINGLETON_DOWNLOADER.to:
-            downloader = ImageDownloader(config.data_dir, max_workers=os.cpu_count(), run=config.run)
+            downloader = ImageDownloader(
+                config.data_dir, max_workers=os.cpu_count(), run=config.run)
         else:
             downloader = None
         self.provider = SDSSDataProvider(
@@ -49,7 +51,8 @@ class Sdss(data.Dataset):
         )
 
         self.num_patches_per_field = (self.cropped_image_shape[0] // self.patch_shape[0]) * \
-                                     (self.cropped_image_shape[1] // self.patch_shape[1])
+                                     (self.cropped_image_shape[1] //
+                                      self.patch_shape[1])
 
     @property
     def num_fields(self) -> int:
@@ -70,16 +73,22 @@ class Sdss(data.Dataset):
             raise ValueError(f"Field with index {field_idx} is None.")
 
         patch_x, patch_y = self._get_patch(field, patch_idx)
-        patch_x, patch_y = Tensor(patch_x), Tensor(patch_y)
+        combined_patch = np.concatenate((patch_x, patch_y), axis=-1)
+
         if self.config.transform:
-            patch_x = self.config.transform(patch_x)
-        if self.config.target_transform:
-            patch_y = self.config.target_transform(patch_y)
+            augmented_patch = self.config.transform(combined_patch)
+            patch_x = augmented_patch[:, :, :patch_x.shape[-1]]
+            patch_y = augmented_patch[:, :, patch_x.shape[-1]:]
+        else:
+            patch_x, patch_y = torch.as_tensor(
+                patch_x), torch.as_tensor(patch_y)
+
         return patch_x, patch_y
 
     def _get_patch(self, field: tuple[np.ndarray, np.ndarray], patch_idx: int) -> tuple[np.ndarray, np.ndarray]:
         x, y = self._get_cropped_data(field)
-        x_patch, y_patch = self.__extract_patch(x, patch_idx), self.__extract_patch(y, patch_idx)
+        x_patch, y_patch = self.__extract_patch(
+            x, patch_idx), self.__extract_patch(y, patch_idx)
         return x_patch, y_patch
 
     def __extract_patch(self, image: np.ndarray, patch_index: int) -> np.ndarray:
@@ -96,8 +105,10 @@ class Sdss(data.Dataset):
 
     def _get_cropped_data(self, field: tuple[np.ndarray, np.ndarray]) -> tuple[np.ndarray, np.ndarray]:
         field_x, field_y = field
-        cropping_width_slice = slice(self.inner_padding, self.inner_padding + self.cropped_image_shape[0])
-        cropping_height_slice = slice(self.inner_padding, self.inner_padding + self.cropped_image_shape[1])
+        cropping_width_slice = slice(
+            self.inner_padding, self.inner_padding + self.cropped_image_shape[0])
+        cropping_height_slice = slice(
+            self.inner_padding, self.inner_padding + self.cropped_image_shape[1])
         copped_x = field_x[cropping_width_slice, cropping_height_slice]
         copped_y = field_y[cropping_width_slice, cropping_height_slice]
         return copped_x, copped_y
